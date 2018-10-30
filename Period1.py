c98a589ca15aa1e04ab6688104a3e51956289163
nicolemoskovitch <nicole.moskovitch@greenwoodcollege.com>
2018-10-30 09:27:01 -0400

    Finished the first task

--- Period1.py
+++ Period1.py
@@ -4,3 +4,15 @@ def hello_print(first_half):
     return first_half+second_half
 
 print(hello_print("Hello"))
+
+a=0
+b=0
+
+def task1():
+    a = int(input("what is the first number?"))
+    b = int(input("what is the second number?"))
+
+    return a * b
+
+print(task1())
+

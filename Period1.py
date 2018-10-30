26cdd6f7566d36d043fae41e2a04414a5ff8aa33
nicolemoskovitch <nicole.moskovitch@greenwoodcollege.com>
2018-10-30 09:32:39 -0400

    Finished the second task

--- Period1.py
+++ Period1.py
@@ -7,6 +7,7 @@ print(hello_print("Hello"))
 
 a=0
 b=0
+c=0
 
 def task1():
     a = int(input("what is the first number?"))
@@ -16,4 +17,12 @@ def task1():
 
 print(task1())
 
+
 def task2():
+    a=int(input("put in a number between 1 and ten to see if it matched the other number"))
+    if a ==3:
+        print("true")
+    if a != 3:
+        print("false")
+
+print(task2())
